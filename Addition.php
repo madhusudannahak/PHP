80cec1b79b5622041cc12dd114e3508747093cad
Madhusudan Nahak <121723803+madhusudannahak@users.noreply.github.com>
2024-05-28 10:49:22 +0530

    Addition

--- Addition.php
+++ Addition.php
@@ -1,29 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<body>
+ </head>
+ <body>
     <form method="post">
-        <input type="text" id="num1" name="num1" required><br>
-        <input type="text" id="num2" name="num2" required><br>
-        <input type="submit" value="submit" id="submit" name="submit">
+        <h2>
+            Enter 1st NO: &nbsp;<input type="text" id="no1" name="no1" autocomplete="off" required ><br>
+            Enter 2st NO: &nbsp;<input type="text" id="no2" name="no2" autocomplete="off" required><br>
+            <input type ="submit" value="submit" id="submit">
+        </h2>
     </form>
 
     <?php
-     if(isset($_POST['submit']) && isset($_POST['num1']) && isset($_POST['num2'])){
-        $num1=$_POST['num1'];
-        $num2=$_POST['num2'];
-        $num1=floatval($num1);
-        $num2=floatval($num2);
-        $sum=$num1+$num2;
-        echo "<h2>result:</h2>";
-        echo "<p>Addition of Number $num1 and $num2 is: $sum</p>";
-     }
+    if(isset($_POST['no1']) && isset($_POST['no2'])){
+        $no1=$_POST['no1'];
+        $no2=$_POST['no2'];
+
+        $no1=floatval($no1);
+        $no2=floatval($no2);
+
+        $sum=$no1+$no2;
+
+        echo "<h2>Result</h2>";
+        echo "<p>Addition of Number $no1 and $no2 is :$sum</p>";
+
+    }
+
     ?>
-</body>
-</html>
-
-
+ </body>
+ </html>
