16ee6cb35cb2f2bf7f2a7b59e2c852d059d1a0eb
Madhusudan Nahak <121723803+madhusudannahak@users.noreply.github.com>
2024-05-28 22:31:02 +0530

    Array index

---
+++ Array index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// Sample array with values
+$array = ["Apple", "Banana", "Orange"];
+
+// Using print_r() to display the array
+echo "Using print_r():<br>";
+print_r($array);
+echo "<br><br>";
+
+// Using var_dump() to display the array
+echo "Using var_dump():<br>";
+var_dump($array);
+?>
+
+</body>
+</html>
